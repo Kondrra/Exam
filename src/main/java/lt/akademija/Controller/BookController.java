2829716e7142cb379081ffb421307d95446e1c8a
Karolis Kondrotas <karolis.kondrotas@gmail.com>
2017-12-21 09:04:04 +0000

    should remove /target + changes

--- src/main/java/lt/akademija/Controller/BookController.java
+++ src/main/java/lt/akademija/Controller/BookController.java
@@ -11,5 +11,46 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@RestController
+@Api(value = "Book")
+@CrossOrigin(origins = "http://localhost:3000/books")
+@RequestMapping(value = "/api")
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping(value = "/products")
+    @ApiOperation(value = "Take a look at books", notes = "Shows books")
+    public List<Book> getBooks(@ApiParam(value = "Search for books")
+                               @RequestParam(value = "search", required = false) String search) {
+        return bookService.getProducts(search);
+    }
+
+    @GetMapping(value = {"/books/{id}", "/admin/books/{id}"})
+    @ApiOperation(value = "Get books", notes = "Returns one book")
+    public Book getBook(@PathVariable String id) {
+        return bookService.getBook(id);
+    }
+
+    @PostMapping(value = "/admin/books/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Create products", notes = "Create a book")
+    public void createBook(@RequestBody CreateBook cmd) {
+        bookService.createProduct(cmd);
+    }
+
+    @PutMapping(value = "/admin/books/{id}")
+    @ApiOperation(value = "Update product", notes = "Updates product information")
+    public void updateBook(@RequestBody CreateBook cmd, @PathVariable String id) {
+        bookService.updateProduct(cmd, id);
+    }
+
+    @DeleteMapping(value = "/admin/books/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation(value = "Delete a book", notes = "Delete one book")
+    public void deleteProduct(@PathVariable String id) {
+        bookService.deleteProduct(id);
+    }
 }
+
--- src/main/java/lt/akademija/Service/BookService.java
+++ src/main/java/lt/akademija/Service/BookService.java
@@ -1,11 +1,11 @@
 package lt.akademija.Service;
 
-import lt.akademija.Core.CreateBook
-import lt.akademija.Core.Book
+import lt.akademija.Core.CreateBook;
+import lt.akademija.Core.Book;
 import lt.akademija.Repository.BookRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,42 +21,38 @@ public class BookService {
 
     @Transactional
     public List<Book> getProducts(String search) {
-        return search == null ? productRepository.findAll() : productRepository.findByTitleOrDescription(search);
+        return search == null ? bookRepository.findAll() : bookRepository.findByTitleOrDescription(search);
     }
 
     @Transactional
-    public Product getProduct(@PathVariable String id) {
-        return productRepository.getOne(Long.parseLong(id));
+    public Book getBook(@PathVariable String id) {
+        return bookRepository.getOne(Long.parseLong(id));
     }
 
     @Transactional
-    public void createProduct(@RequestBody CreateProductCmd cmd) {
-        Product product = new Product();
-        product.setTitle(cmd.getTitle());
-        product.setDescription(cmd.getDescription());
-        product.setImage(cmd.getImage());
-        product.setQuantity(cmd.getQuantity());
-        product.setPrice(cmd.getPrice());
-        productRepository.save(product);
+    public void createProduct(@RequestBody CreateBook cmd) {
+        Book book = new Book();
+        book.setTitle(cmd.getTitle());
+        book.setAuthor(cmd.getAuthor());
+        book.setImage(cmd.getImage());
+        book.setNumberofpages(cmd.getNumberofpages());
+        bookRepository.save(book);
     }
 
     @Transactional
-    public void updateProduct(@RequestBody CreateProductCmd cmd, @PathVariable String id) {
-        Product product = productRepository.getOne(Long.parseLong(id));
-        if (product != null) {
-            BeanUtils.copyProperties(cmd, product);
-            productRepository.save(product);
+    public void updateProduct(@RequestBody CreateBook cmd, @PathVariable String id) {
+        Book book = bookRepository.getOne(Long.parseLong(id));
+        if (book != null) {
+            BeanUtils.copyProperties(cmd, book);
+            bookRepository.save(book);
         }
     }
 
     @Transactional
     public void deleteProduct(@PathVariable String id) {
-        userRepository.findAll().forEach(user -> user.removeProduct(id));
-        productRepository.delete(Long.parseLong(id));
+        bookRepository.delete(Long.parseLong(id));
     }
 
 
-
-
 }
-}
+
